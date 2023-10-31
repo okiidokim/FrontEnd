3b4021bca273f625104789cb2ad7066e47d72bd7
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-10-31 16:34:09 +0900

    Create: 초기설정 - 폴더구조 형성 및 라우팅

--- catch-culture-frontend/src/main.jsx
+++ catch-culture-frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import router from './router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ catch-culture-frontend/src/pages/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// const Main = () => {
+//   return <>Main 페이지</>;
+// };
+
+function Main() {
+  return <div>Main 페이지</div>;
+}
+
+export default Main;
