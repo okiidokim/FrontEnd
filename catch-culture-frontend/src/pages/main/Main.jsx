8f90794279bd116475c40e510c09fa4f1541032e
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-04 16:45:59 +0900

    Design: 메인페이지 - 문화 행사 카드 디자인

--- catch-culture-frontend/src/pages/main/Main.jsx
+++ catch-culture-frontend/src/pages/main/Main.jsx
@@ -5,6 +5,7 @@ import SearchImg from '../../assets/images/search/searchIcon.png';
 import BannerImg1 from '../../assets/images/main/banner1.png';
 import BannerImg2 from '../../assets/images/main/banner2.png';
 import BannerImg3 from '../../assets/images/main/banner3.png';
+import CardImg from '../../assets/images/main/card.png';
 
 // 슬라이더
 import { SwiperSlide } from 'swiper/react';
@@ -56,7 +57,28 @@ function Main() {
           </S.EventHeader>
 
           <S.EventList>
-            <S.EventCard></S.EventCard>
+            <S.EventCard>
+              <S.EventCardImgWrapper>
+                <S.EventCardImgTag>D-7</S.EventCardImgTag>
+                <S.EventCardImg src={CardImg} alt="카드 이미지" />
+              </S.EventCardImgWrapper>
+
+              <S.EventCardContentWrapper>
+                <S.EventCardTitle>응봉산 개나리 축제</S.EventCardTitle>
+                <S.EventCardLocation>성동구 응봉산</S.EventCardLocation>
+                <S.EventCardDate>2023.09.24 ~ 2023.10.24</S.EventCardDate>
+                <S.EventCardInfo>
+                  <S.EventCardInfoViewWrapper>
+                    <S.EventCardInfoViewIcon />
+                    <S.EventCardInfoViewCnt>257만</S.EventCardInfoViewCnt>
+                  </S.EventCardInfoViewWrapper>
+                  <S.EventCardInfoHeartWrapper>
+                    <S.EventCardInfoHeartIcon />
+                    <S.EventCardInfoHeartCnt>246만</S.EventCardInfoHeartCnt>
+                  </S.EventCardInfoHeartWrapper>
+                </S.EventCardInfo>
+              </S.EventCardContentWrapper>
+            </S.EventCard>
           </S.EventList>
         </S.EventWrapper>
       </S.MainWrapper>
--- catch-culture-frontend/src/pages/main/style.jsx
+++ catch-culture-frontend/src/pages/main/style.jsx
@@ -1,5 +1,7 @@
 import { styled } from 'styled-components';
 import { Swiper } from 'swiper/react';
+// 아이콘
+import { AiFillHeart, AiFillEye } from 'react-icons/ai';
 
 // 메인
 export const MainWrapper = styled.div`
@@ -111,5 +113,95 @@ export const EventHeaderMore = styled.div`
   font-weight: 700;
   line-height: normal;
 `;
-export const EventList = styled.div``;
+
+// 추천 행사 목록
+export const EventList = styled.div`
+  margin-top: 20px;
+  display: grid;
+  grid-template-rows: 2fr;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 22px;
+`;
+
+// 문화 행사 카드
 export const EventCard = styled.div``;
+
+export const EventCardImgWrapper = styled.div`
+  position: relative;
+`;
+
+export const EventCardImgTag = styled.div`
+  display: flex;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  justify-content: center;
+  align-items: center;
+  width: 33px;
+  height: 14px;
+  background-color: #018c0d;
+  border-radius: 10px;
+  color: #fff;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const EventCardImg = styled.img`
+  width: 155px;
+  height: 155px;
+  border-radius: 10px;
+`;
+
+// 문화 행사 카드 - 내용
+export const EventCardContentWrapper = styled.div`
+  margin-top: 5px;
+  color: #acacac;
+`;
+
+// 문화 행사 카드 - 내용 - 타이틀
+export const EventCardTitle = styled.div`
+  color: #000;
+  font-size: 18px;
+  font-weight: 700;
+  margin-bottom: 8px;
+`;
+
+// 문화 행사 카드 - 내용 - 위치
+export const EventCardLocation = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  margin-bottom: 5px;
+`;
+
+// 문화 행사 카드 - 내용 - 기간
+export const EventCardDate = styled.div`
+  font-size: 13px;
+  font-weight: 500;
+  margin-bottom: 6px;
+`;
+
+// 문화 행사 카드 - 내용 - 조회수 + 좋아요수
+export const EventCardInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  font-weight: 500;
+`;
+export const EventCardInfoViewWrapper = styled.div`
+  display: flex;
+  margin-right: 8px;
+`;
+export const EventCardInfoViewIcon = styled(AiFillEye)`
+  margin-right: 2px;
+`;
+export const EventCardInfoViewCnt = styled.div``;
+export const EventCardInfoHeartWrapper = styled.div`
+  display: flex;
+`;
+export const EventCardInfoHeartIcon = styled(AiFillHeart)`
+  margin-right: 2px;
+`;
+export const EventCardInfoHeartCnt = styled.div``;
