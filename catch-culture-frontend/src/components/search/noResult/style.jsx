516ea08402805e2ca3ba9a0f25fc3e28112720de
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-18 00:47:27 +0900

    Design: 정렬 selector 위치 고정

--- catch-culture-frontend/src/components/search/noResult/style.jsx
+++ catch-culture-frontend/src/components/search/noResult/style.jsx
@@ -2,7 +2,6 @@ import { styled } from 'styled-components';
 
 export const NoResultWrapper = styled.div`
   display: flex;
-
   flex-direction: column;
   justify-content: center;
   align-items: center;
