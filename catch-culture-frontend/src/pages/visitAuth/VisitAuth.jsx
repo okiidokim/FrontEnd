0d308cecdbb44537c85cde250a597a612f71de80
jinwoo1234 <skdiwlsdn5@naver.com>
2023-11-21 00:26:12 +0900

    Design: 방문인증 페이지 꾸미기

--- catch-culture-frontend/src/pages/visitAuth/VisitAuth.jsx
+++ catch-culture-frontend/src/pages/visitAuth/VisitAuth.jsx
@@ -2,7 +2,13 @@ import React, { useMemo, useState } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 import * as S from './VisitAuthStyle';
 
+import Backitem from '../../components/Backitem';
+import UploadBox from '../../components/uploadImg/UploadBox'
+
 function VisitAuth() {
+
+    const [isAuthenticated, setIsAuth] = useState();
+
     const onClickBackButton = () => {
         navigate(-1);
     }
@@ -10,14 +16,26 @@ function VisitAuth() {
     return (
         <S.Wrapper>
             <S.Header>
-                <S.BackButton onClick={onClickBackButton}> 
+                <Backitem> 
                     <IoIosArrowBack />
-                </S.BackButton>
-                
+                </Backitem>
             </S.Header>
-            {/* <S.InfoArea>
 
-            </S.InfoArea> */}
+            <S.Container>
+                <S.TitleArea>
+                    더 크림 갤러리
+                </S.TitleArea>
+                <S.SubTitle>
+                    사진 등록 (최대 3개)
+                </S.SubTitle>
+                <UploadBox/>
+            </S.Container>
+
+            <S.ButtonSection>           
+                <button onClick={''} disabled={isAuthenticated} style={ isAuthenticated ? {backgroundColor: '#A7A7A7'} : {backgroundColor: '#018C0D'}}>
+                    방문 인증 요청
+                </button>
+            </S.ButtonSection>
         </S.Wrapper>
     )
 }
