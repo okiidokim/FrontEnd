a2372b54123d2776f17ed6b62c2a02d5ae0c968f
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-24 04:37:54 +0900

    Feat: 제보하기 두번째 페이지 - text input 구현

--- catch-culture-frontend/src/pages/reportPage/report2/Report2.jsx
+++ catch-culture-frontend/src/pages/reportPage/report2/Report2.jsx
@@ -17,6 +17,44 @@ function Report2() {
         <ReportHeader ReportPaginationIcon={ReportPaginationIcon} />
 
         <S.ReportContent>
+          <S.ReportRequired>* 는 필수 입력 사항입니다</S.ReportRequired>
+          <S.ReportForm>
+            {/* 행사명 */}
+            <S.ReportEvent>
+              <S.ReportEventTitle>행사명 *</S.ReportEventTitle>
+              <S.ReportEventInput
+                type="text"
+                placeholder="행사명을 입력해주세요."
+              />
+            </S.ReportEvent>
+
+            {/* SNS 주소 */}
+            <S.ReportEvent>
+              <S.ReportEventTitle>SNS 주소</S.ReportEventTitle>
+              <S.ReportEventInput
+                type="text"
+                placeholder="행사 관련 SNS 주소를 입력해주세요."
+              />
+            </S.ReportEvent>
+
+            {/* 전화번호 */}
+            <S.ReportEvent>
+              <S.ReportEventTitle>SNS 주소</S.ReportEventTitle>
+              <S.ReportEventInput
+                type="text"
+                placeholder="행사 담당 전화번호를 입력해주세요."
+              />
+            </S.ReportEvent>
+
+            {/* 오시는 길 */}
+            <S.ReportEvent>
+              <S.ReportEventTitle>오시는 길</S.ReportEventTitle>
+              <S.ReportEventInput
+                type="text"
+                placeholder="행사에 쉽게 올 수 있는 방법을 설명해주세요."
+              />
+            </S.ReportEvent>
+          </S.ReportForm>
           {/* 제보하기 버튼 */}
           <Link to="/report3">
             <S.ReportButton>제보하기</S.ReportButton>
--- catch-culture-frontend/src/pages/reportPage/report2/style.jsx
+++ catch-culture-frontend/src/pages/reportPage/report2/style.jsx
@@ -1,90 +1,75 @@
 import { styled } from 'styled-components';
-import { Swiper } from 'swiper/react';
 
 export const ReportWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-export const ReportHeader = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 10px;
-`;
-
-export const ReportHeaderTitle = styled.div`
-  display: flex;
-  margin-left: 10px;
-
-  color: #000;
-  font-size: 20px;
-  font-weight: 700;
-  line-height: normal;
-`;
-
-export const ReportHeaderSlide = styled.div`
-  display: flex;
-`;
-
 export const ReportContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-top: 12px;
 `;
 
-export const ReportLogo = styled.img`
+// 필수 사항 공지
+export const ReportRequired = styled.div`
   display: flex;
-  width: 300px;
-  margin-top: 50px;
-`;
-
-export const ReportWelcome = styled.div`
-  margin-top: 30px;
-  display: flex;
-  color: #018c0d;
-  text-align: center;
-  font-size: 16px;
-  font-weight: 700;
-  line-height: normal;
-`;
-
-export const ReportNotice = styled.div`
-  margin-top: 5px;
-  display: flex;
-  color: #000;
-  text-align: center;
-  font-size: 12px;
-  font-weight: 400;
-  line-height: normal;
-`;
-
-export const ReportRedNotice = styled.div`
-  margin-top: 30px;
-  width: 296px;
+  justify-content: flex-end;
+  width: 100%;
   color: #f00;
-  font-size: 12px;
-  font-weight: 400;
+  text-align: center;
+  font-size: 10px;
+  font-weight: 300;
   line-height: normal;
 `;
 
-// 제보 공지 타이틀
-export const ReportTitle = styled.div`
-  margin-top: 30px;
-  width: 298px;
+// 폼 데이터
+export const ReportForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+// 입력 항목
+export const ReportEvent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+`;
+
+export const ReportEventTitle = styled.div`
+  display: flex;
+  color: #000;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.3px;
+  margin-bottom: 16px;
+`;
+
+export const ReportEventInput = styled.input`
+  display: flex;
+  width: 284px;
+  text-decoration: none;
+  border: none;
+  border-bottom: 1px solid #777;
   color: #018c0d;
   font-size: 12px;
   font-weight: 400;
   line-height: normal;
-`;
+  letter-spacing: -0.3px;
+  padding: 8px;
 
-// 제보 공지 넘버링 내용
-export const ReportNumber = styled.div`
-  margin-top: 5px;
-  width: 298px;
-  color: #000;
-  font-size: 12px;
-  font-weight: 400;
-  line-height: 20px;
+  /* &::placeholder {
+    color: rgba(1, 140, 13, 0.56);
+  } */
+
+  &:focus {
+    border: none;
+    outline: 2px solid #71be77;
+    border-radius: 3px;
+  }
 `;
 
 // 제보 버튼
@@ -105,23 +90,4 @@ export const ReportButton = styled.div`
   line-height: normal;
   margin-top: 70px;
   cursor: pointer;
-`;
-
-export const MySwiper = styled(Swiper)`
-  width: 336px;
-  height: auto;
-  padding-bottom: 30px;
-
-  // Pagination 버튼의 비활성 상태 스타일
-  .swiper-pagination-bullet {
-    width: 8x;
-    height: 8px;
-  }
-
-  // Pagination 버튼의 활성 상태 스타일
-  .swiper-pagination-bullet-active {
-    background-color: #018c0d;
-    width: 8x;
-    height: 8px;
-  }
 `;
