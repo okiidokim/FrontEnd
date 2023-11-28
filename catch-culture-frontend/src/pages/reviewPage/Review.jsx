7444eea60866e2090c0bf8e0e98669cde179adc6
jinwoo1234 <skdiwlsdn5@naver.com>
2023-11-28 15:43:26 +0900

    Fix: 리뷰 작성 api 연동

--- catch-culture-frontend/src/pages/reviewPage/Review.jsx
+++ catch-culture-frontend/src/pages/reviewPage/Review.jsx
@@ -9,16 +9,20 @@ import SetRating from './setRating/SetRating.jsx';
 // api
 import axios from '../../api/axios';
 import { TbRating18Plus } from 'react-icons/tb';
+import { useNavigate, useParams } from 'react-router-dom';
 
-function Review ( params ) {
-    let title = "test"; // params.data.title
-    let eventId = "10"; //params.data.eventId
+function Review ( ) {
+    const params = useParams();
+    let title = "test";
+    const eventId = params.id;
 
     const [disabled, setDisabled] = useState(false);
     
     const [rating, setRating] = useState();
     const [formData, setFormData] = useState();
     const [description, setDescription] = useState("");
+
+    const navigate = useNavigate();
 
     const handleRating = (rating) => {
         setRating(rating);
@@ -42,43 +46,33 @@ function Review ( params ) {
             try {
                 console.log(formData)
                 if(!(formData.get('file') === null)) {
+                    const requestBody = new FormData();
+                    
+                    let data = {
+                        description: description,
+                        rating: rating
+                    }
+                    
 
-                    const reviewDetail = new FormData();
-                    let data = new Object();
-                    // reviewDetail.append("description", description);
-                    // reviewDetail.append("rating", rating);
-                    data.description = description;
-                    data.rating = rating;
+                    requestBody.append('file', formData.get('file'));
+                    requestBody.append('reviewDetail', new Blob([JSON.stringify(data)], { type: "application/json" }));
+                    //  for (var key of requestBody.entries()) {
+                    //      console.log(key[0] + ', ' + key[1]);
+                    //  }
                     
-                    
-                    let des = `description: ${description}`;
-                    let rat = `rating: ${rating}`;
-                    // formData.append("reviewDetail", new Blob([des, rat], { type: 'application/json' }));
-                    formData.append("reviewDetail", JSON.stringify(data), );
-                     for (var key of formData.entries()) {
-                         console.log(key[0] + ', ' + key[1]);
-                     }
-                     console.log(JSON.stringify(data))
-                     console.log(formData.get('reviewDetail'))
-                     
-                
-                    const request = await axios.post(
-                        `review/${parseInt(eventId)}/my-review`,
-                        // {
-                            formData,
-                            // file: formData.get('file'),
-                            // reviewDetail: reviewDetail.get('description')
-                        // },
-                        {
-                            headers: [{
-                                'Content-Type': 'multipart/form-data',
-                            }],
-                        }
+                    const request = await axios({
+                        method: "POST",
+                        url: `review/${eventId}/my-review`,
+                        mode: "cors",
+                        headers: {
+                            'Content-Type': 'multipart/form-data',     
+                        },
+                        data: requestBody,
+                    }).then(
+                        navigate(`/event/${eventId}`)
                     )
-                    console.log(request);
+                    
                 }
-
-                
             } catch (e) {
                 console.log(e);
             }
