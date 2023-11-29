d5358a66122e89350540dfb3f08fcf05f417d7fd
okiidokim <scw09268@dgu.ac.kr>
2023-11-30 06:16:23 +0900

    Feat : 관리자 페이지 구현 중
    제보 페이지 post api 연동 제외 구현 완료
    방문 인증 리스트 페이지 구현 완료
    방문 인증 상세 페이지 현재 데이터 없어서 보류 중
    (VisitAuth api 없어서 현재 데이터 생성 불가)

---
+++ catch-culture-frontend/src/pages/admin/AdminReport.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import Backitem from '../../components/Backitem';
+import axios from '../../api/axios';
+import { useParams } from 'react-router-dom';
+import './AdminReport.css';
+import { NavLink } from 'react-router-dom';
+
+function ReportInfo(params) {
+  const d = params.data;
+
+  return (
+    <div className="reportinfowrap">
+      <hr />
+      <div>
+        <img className="reportimg" src={d.storedFileUrl}></img>
+      </div>
+      <hr />
+      <div className="reportcontent">
+        <div className="reportname">
+          <p className="textname">행사명</p>{' '}
+          <p className="textcontent">{d.title} </p>
+        </div>
+        <div className="reportplace">
+          <p className="textname">행사 위치</p>{' '}
+          <p className="textcontent">{d.place}</p>
+        </div>
+        <div className="reportduration">
+          <p className="textname">행사 기간</p>
+          <p className="textcontent">
+            {d.startDate} - {d.endDate}
+          </p>
+        </div>
+        <div className="reportdes">
+          <p className="textname">행사 상세 내용</p>
+          <p className="textcontent">{d.description}</p>
+        </div>
+        <div className="reportisfree">
+          <p className="textname">가격 정보</p>
+          <p className="textcontent">
+            {d.isFree === true ? <>무료</> : <>유료</>}
+          </p>
+        </div>
+      </div>
+      <div className="regisbuttonrow">
+        <NavLink to="/reportauth/list">
+          <div className="regis">등록</div>
+        </NavLink>
+        <NavLink to="/reportauth/list">
+          <div className="noregis">미등록</div>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+let data = 'sample';
+export default function AdminReport() {
+  const params = useParams();
+  const reportId = params.id;
+  const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `/admin/event-report/${parseInt(reportId)}`
+        );
+        data = {
+          reportId: reportId,
+          storedFileUrl: res.data.culturalEventDetail.storedFileUrl,
+          startDate: res.data.culturalEventDetail.startDate,
+          endDate: res.data.culturalEventDetail.endDate,
+          title: res.data.culturalEventDetail.title,
+          place: res.data.culturalEventDetail.place,
+          cate: res.data.culturalEventDetail.category,
+          description: res.data.culturalEventDetail.description
+            ? res.data.culturalEventDetail.description
+            : 'null',
+          eventLink: res.data.culturalEventDetail.reservationLink,
+          wayto: res.data.culturalEventDetail.wayToCome,
+          sns: res.data.culturalEventDetail.sns,
+          telephone: res.data.culturalEventDetail.telephone,
+          isFree: res.data.culturalEventDetail.isFree,
+          latitude: res.data.culturalEventDetail.latitude,
+          longitude: res.data.culturalEventDetail.longitude,
+        };
+        setNickname(res.data.nickname);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="adminvisitwrap">
+      <Backitem />
+      <div className="adminvisitcontent">
+        <div className="reporterbox">
+          <p>제보자 : </p> <p className="reporter">{nickname}</p>
+        </div>
+        <ReportInfo data={data} />
+      </div>
+    </div>
+  );
+}
---
+++ catch-culture-frontend/src/pages/admin/AdminVisit.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Backitem from '../../components/Backitem';
+import * as dayjs from 'dayjs';
+
+export default function AdminVisit() {
+  return (
+    <div className="adminvisitwrap">
+      <Backitem />
+      <div className="adminvisitcontent">방문 인증 요청 상세 페이지</div>
+    </div>
+  );
+}
