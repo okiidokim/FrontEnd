e6d5374e45bf4094f2b085bd8f45b0218e2381de
jinwoo1234 <skdiwlsdn5@naver.com>
2023-11-29 18:54:59 +0900

    Feat: kakao Map api 연결

--- catch-culture-frontend/src/pages/map/Map.jsx
+++ catch-culture-frontend/src/pages/map/Map.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as S from './style';
 
+const { kakao } = window;
 
 function Map() {
-  return <div>Map 페이지</div>;
+
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(37.558288, 127.000173),
+      level: 3
+    };
+    var map = new kakao.maps.Map(container, options);
+  }, [])
+
+  return (
+    <S.Map id="map" style={{width:'100%', height:'100%'}}>
+
+    </S.Map>
+  );
 }
 
 export default Map;
---
+++ catch-culture-frontend/src/pages/map/style.jsx
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const Map = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    flex-grow: 1;
+    flex-shrink: 1;
+`;
