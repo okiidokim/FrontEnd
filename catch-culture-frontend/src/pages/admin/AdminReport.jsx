0360682b6bb42233f74d1e7442dcf72fd855d65c
okiidokim <scw09268@dgu.ac.kr>
2023-11-30 18:04:45 +0900

    Fix : 관리자 페이지 제보 부분 구현 중
    post api 제외 찐 구현 완

--- catch-culture-frontend/src/pages/admin/AdminReport.jsx
+++ catch-culture-frontend/src/pages/admin/AdminReport.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Backitem from '../../components/Backitem';
 import axios from '../../api/axios';
 import { useParams } from 'react-router-dom';
@@ -10,10 +10,27 @@ import {
   TbNotes,
   TbCash,
   TbCalendarEvent,
+  TbCategory,
+  TbBrandInstagram,
+  TbPhone,
+  TbRoadSign,
+  TbLink,
 } from 'react-icons/tb';
 
 function ReportInfo(params) {
   const d = params.data;
+
+  const [isShowMore, setIsShowMore] = useState(false);
+  const textLimit = 78;
+
+  const descriptionCut = useMemo(() => {
+    const shortView = `${d.description}`.slice(0, textLimit);
+    if (`${d.description}`.length > textLimit) {
+      if (isShowMore) return d.description;
+      else return shortView;
+    }
+    return d.description;
+  }, [isShowMore]);
 
   return (
     <div className="reportinfowrap">
@@ -43,15 +60,36 @@ function ReportInfo(params) {
             행사 기간
           </p>
           <p className="textcontent">
-            {d.startDate} - {d.endDate}
+            {d.startDate} ~ {d.endDate}
           </p>
+        </div>
+        <div className="reportcate">
+          <p className="textname">
+            <TbCategory />
+            행사 카테고리
+          </p>
+          <p className="textcontent">{d.cate}</p>
         </div>
         <div className="reportdes">
           <p className="textname">
             <TbNotes />
             행사 상세 내용
           </p>
-          <p className="textcontent">{d.description}</p>
+          <div
+            className="textcontent"
+            onClick={() => setIsShowMore(!isShowMore)}
+          >
+            {descriptionCut}
+            <span
+              style={{ color: 'grey', fontWeight: '100', cursor: 'pointer' }}
+            >
+              {`${d.description}`.length > textLimit
+                ? isShowMore
+                  ? '닫기'
+                  : '...더보기'
+                : null}
+            </span>
+          </div>
         </div>
         <div className="reportisfree">
           <p className="textname">
@@ -62,8 +100,52 @@ function ReportInfo(params) {
             {d.isFree === true ? <>무료</> : <>유료</>}
           </p>
         </div>
-        <div className="otherinfos"></div>
+        {d.sns === null ? (
+          <></>
+        ) : (
+          <div className="reportsnsinfo">
+            <p className="textname">
+              <TbBrandInstagram />
+              SNS 계정 정보
+            </p>
+            <p className="textcontent">{d.sns}</p>
+          </div>
+        )}
+        {d.telephone === null ? (
+          <></>
+        ) : (
+          <div className="reportreservelink">
+            <p className="textname">
+              <TbPhone />
+              전화번호
+            </p>
+            <p className="textcontent">{d.telephone}</p>
+          </div>
+        )}
+        {d.wayto === null ? (
+          <></>
+        ) : (
+          <div className="reportwayto">
+            <p className="textname">
+              <TbRoadSign />
+              오시는 길
+            </p>
+            <p className="textcontent">{d.wayto}</p>
+          </div>
+        )}
+        {d.eventLink === null ? (
+          <></>
+        ) : (
+          <div className="reportreservelink">
+            <p className="textname">
+              <TbLink />
+              예약 링크
+            </p>
+            <p className="textcontent">{d.eventLink}</p>
+          </div>
+        )}
       </div>
+
       <div className="regisbuttonrow">
         <NavLink to="/reportauth/list">
           <div className="regis">등록</div>
@@ -96,16 +178,14 @@ export default function AdminReport() {
           title: res.data.culturalEventDetail.title,
           place: res.data.culturalEventDetail.place,
           cate: res.data.culturalEventDetail.category,
-          description: res.data.culturalEventDetail.description
-            ? res.data.culturalEventDetail.description
-            : 'null',
-          eventLink: res.data.culturalEventDetail.reservationLink,
+          description: res.data.culturalEventDetail.description,
           wayto: res.data.culturalEventDetail.wayToCome,
           sns: res.data.culturalEventDetail.sns,
           telephone: res.data.culturalEventDetail.telephone,
           isFree: res.data.culturalEventDetail.isFree,
           latitude: res.data.culturalEventDetail.latitude,
           longitude: res.data.culturalEventDetail.longitude,
+          eventLink: res.data.culturalEventDetail.reservationLink,
         };
         setNickname(res.data.nickname);
       } catch (e) {
