89a1ae7cc52346c9c8cab48d0dc2255a0b26c1db
jinwoo1234 <skdiwlsdn5@naver.com>
2023-11-30 17:46:38 +0900

    Feat: Map 페이지 카테고리 선택 구현

--- catch-culture-frontend/src/pages/map/Map.jsx
+++ catch-culture-frontend/src/pages/map/Map.jsx
@@ -1,45 +1,91 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
+import { useLocation } from 'react-router-dom';
 
 import axios from '../../api/axios'
+import CategorySelector from '../../components/categorySelector/CategorySelector';
 
 const { kakao } = window;
 
 function Map() {
-  var map;
+  const [map, setMap] = useState();
+  const [markers, setMarkers] = useState([]);
+
+  const [eventData, setEventData] = useState();
+  const { state } = useLocation();
+  // state 값 유무에 따른 초기값 설정
+
+  const category = state && state.category;
+  const initialCategories = category ? category : [];
+  const [selectedCategories, setSelectedCategories] = useState(initialCategories);
+
 
   useEffect(() => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(37.558288, 127.000173),
-      level: 3
-    };
-    map = new kakao.maps.Map(container, options);
+    initMap();
     fetchMapData();
   }, [])
 
-  const fetchMapData = async() => {
-    const response = await axios.get(
-      `cultural-event/map`
-    )
+  const initMap = () => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(37.558288, 127.000173),
+      level: 4
+    };
+    setMap(new kakao.maps.Map(container, options));
+  }
 
-    console.log(response.data);
+  const fetchMapData = async() => {
+    try {
+      const response = await axios.get(
+        `cultural-event/map`
+      )
+      handleEventData(response.data)
+    } catch(e) {
+
+    }
+  }
+
+  const handleEventData = (data) => {
+    setEventData(data)
+  }
+
+  useEffect(() => {
+    makeMarker(eventData);
+  }, [eventData, selectedCategories])
+
+  const makeMarker = (data) => {
+    clearMarker();
+    if(!data) {
+      return;
+    }
 
     // 마커 생성
-    for(var i = 0; i < response.data.length; i++) {
-      var marker = new kakao.maps.Marker({
-        map: map, // 마커를 표시할 지도
-        position: new kakao.maps.LatLng(response.data[i].latitude, response.data[i].longitude) // 마커의 위치
-      });
+    for(var i = 0; i < data.length; i++) {
+      if(selectedCategories.includes(data[i].category)) {
+        var marker = new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(data[i].latitude, data[i].longitude) // 마커의 위치
+        });
+        marker.setMap(map);
 
-      // 마커에 표시할 인포윈도우 설정
-      var infowindow = new kakao.maps.InfoWindow({
-        content: response.data[i].title // 인포윈도우에 표시할 내용
-      });
+        // 마커에 표시할 인포윈도우 설정
+        var infowindow = new kakao.maps.InfoWindow({
+          content: data[i].title // 인포윈도우에 표시할 내용
+        });
 
-      kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-      kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+        kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+        kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+
+        markers.push(marker);
+      }
     }
+  }
+
+  function clearMarker() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    } 
+    // 마커 배열 초기화
+    markers.splice(0);
   }
 
   // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
@@ -57,9 +103,15 @@ function Map() {
   }
 
   return (
-    <S.Map id="map" style={{width:'100%', height:'100%'}}>
+    <>
+      <S.Map id="map" style={{width:'100%', height:'100%'}}>
 
-    </S.Map>
+      </S.Map>
+      <CategorySelector
+        selectedCategories={selectedCategories}
+        setSelectedCategories={setSelectedCategories}
+      />
+    </>
   );
 }
 
