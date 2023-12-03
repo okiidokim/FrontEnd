652e08aac86c2fac00d50c7d3086496b4798514a
jinwoo1234 <skdiwlsdn5@naver.com>
2023-12-03 21:48:52 +0900

    Design: 자잘한 오류 수정

--- catch-culture-frontend/src/components/ReviewCard/ReviewCard.jsx
+++ catch-culture-frontend/src/components/ReviewCard/ReviewCard.jsx
@@ -122,7 +122,8 @@ export default function ReviewCard(data) {
             다시 원래대로 실행돼서 복구
           .substr(0,data.data.storedFileUrl.length-2)
         */}
-        <S.RvImg src={data.data.storedFileUrl[0] == null ? null : data.data.storedFileUrl} style={{display : data.data.storedFileUrl == null ? 'none': "flex"}}/>
+        {console.log(data.data.storedFileUrl[0] == null)}
+        <S.RvImg src={data.data.storedFileUrl[0] == null ? null : data.data.storedFileUrl} style={{display : data.data.storedFileUrl[0] == null ? 'none': "flex"}}/>
         <S.RvComment>{data.data.description}</S.RvComment>
       </S.ReviewRow>
       <S.Star>{printStar(data.data.rating)}</S.Star>
