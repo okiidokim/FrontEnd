5ab700948c97fef62b1f7803925e0f3ed50656be
okiidokim <scw09268@dgu.ac.kr>
2023-12-06 16:44:35 +0900

    Fix : DeleteModal 중첩 중괄호 코드 스멜 수정

--- catch-culture-frontend/src/components/ReviewCard/deleteModal/DeleteModal.jsx
+++ catch-culture-frontend/src/components/ReviewCard/deleteModal/DeleteModal.jsx
@@ -1,48 +1,40 @@
-import * as S from './style'
+import * as S from './style';
 
-import axios from '../../../api/axios'
+import axios from '../../../api/axios';
 
 export const DeleteModal = (params) => {
-
-    const onClickDelete = async () => {
-        try {
-            await axios.delete(
-                `review/${parseInt(params.EventId)}/my-review`,
-                {
-                    data:{reviewId: params.reviewId},
-                    headers: {
-                        'Content-Type': 'application/json', 
-                    },
-                },
-            )
-            {params.fetchMyReview()}
-            {params.setModal()}
-        } catch (e) {
-            console.log(e);
-        }
+  const onClickDelete = async () => {
+    try {
+      await axios.delete(`review/${parseInt(params.EventId)}/my-review`, {
+        data: { reviewId: params.reviewId },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      params.fetchMyReview();
+      params.setModal();
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const onClickCancle = () => {
-        params.setModal();
-    }
+  const onClickCancle = () => {
+    params.setModal();
+  };
 
-    return (
-        <S.ModalBackdrop>
-            <S.ModalContainer>
-                <S.ModalView>
-                    <p>리뷰를 삭제 하시겠습니까?</p>
-                    <S.ButtonSection>
-                        <S.MyBtn onClick={onClickCancle}>
-                            취소
-                        </S.MyBtn>
-                        <S.MyBtn onClick={onClickDelete}>
-                            삭제
-                        </S.MyBtn>
-                    </S.ButtonSection>
-                </S.ModalView>
-            </S.ModalContainer>
-        </S.ModalBackdrop>
-    )
-}
+  return (
+    <S.ModalBackdrop>
+      <S.ModalContainer>
+        <S.ModalView>
+          <p>리뷰를 삭제 하시겠습니까?</p>
+          <S.ButtonSection>
+            <S.MyBtn onClick={onClickCancle}>취소</S.MyBtn>
+            <S.MyBtn onClick={onClickDelete}>삭제</S.MyBtn>
+          </S.ButtonSection>
+        </S.ModalView>
+      </S.ModalContainer>
+    </S.ModalBackdrop>
+  );
+};
 
 export default DeleteModal;
