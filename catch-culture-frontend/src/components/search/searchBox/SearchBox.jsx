69d7466d65a2b9ffa53f0263ec44c846998b8704
okiidokim <scw09268@dgu.ac.kr>
2023-12-06 17:14:17 +0900

    Fix : SortSelector 코드 스멜 수정

--- catch-culture-frontend/src/components/search/searchBox/SearchBox.jsx
+++ catch-culture-frontend/src/components/search/searchBox/SearchBox.jsx
@@ -1,34 +1,36 @@
 import React, { useState } from 'react';
 import * as S from './style';
+import PropTypes from 'prop-types';
 
 // 아이콘
 import SearchImg from '../../../assets/images/search/searchIcon.png';
-import { Link } from 'react-router-dom';
 
 function SearchBox(props) {
+  SearchBox.propTypes = {
+    keyword: PropTypes.any,
+    width: PropTypes.any,
+  };
   const initialKeyword = props.keyword ? props.keyword : '';
   const [search, setSearch] = useState(initialKeyword);
 
-  const onChange = e => {
+  const onChange = (e) => {
     setSearch(e.target.value);
   };
 
   return (
-    <>
-      <S.HeaderSearchWrapper>
-        <S.HeaderSearch
-          type="text"
-          value={search}
-          onChange={onChange}
-          placeholder="다양한 행사를 검색해보세요"
-          width={props.width}
-        />
+    <S.HeaderSearchWrapper>
+      <S.HeaderSearch
+        type="text"
+        value={search}
+        onChange={onChange}
+        placeholder="다양한 행사를 검색해보세요"
+        width={props.width}
+      />
 
-        <S.StyledLink to="/search" state={{ keyword: search }}>
-          <S.HeaderSearchImg src={SearchImg} alt="검색 이미지" />
-        </S.StyledLink>
-      </S.HeaderSearchWrapper>
-    </>
+      <S.StyledLink to="/search" state={{ keyword: search }}>
+        <S.HeaderSearchImg src={SearchImg} alt="검색 이미지" />
+      </S.StyledLink>
+    </S.HeaderSearchWrapper>
   );
 }
 
