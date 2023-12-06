56b4d0cd256436454b2f80e1fb708c472bea9c80
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-12-07 00:20:49 +0900

    Fix: 제보하기 페이지 - 이미지 안보이는 문제 해결

--- catch-culture-frontend/src/components/reportPage/reportHeader/ReportHeader.jsx
+++ catch-culture-frontend/src/components/reportPage/reportHeader/ReportHeader.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import * as S from './style';
 import Backitem from '../../Backitem';
 
-function ReportHeader(ReportPaginationIcon) {
+function ReportHeader({ReportPaginationIcon}) {
+
+  console.log(ReportPaginationIcon);
   return (
     <>
       <Backitem />
@@ -14,5 +16,9 @@ function ReportHeader(ReportPaginationIcon) {
     </>
   );
 }
+
+ReportHeader.propTypes = {
+  ReportPaginationIcon: PropTypes.string.isRequired 
+};
 
 export default ReportHeader;
