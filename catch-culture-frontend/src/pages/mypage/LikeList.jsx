71082cb34bb534860323da8f65ef30f1e227cd78
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-12-06 16:39:01 +0900

    Fix: 마이페이지 북마크 API 로그인 예외처리

--- catch-culture-frontend/src/pages/mypage/LikeList.jsx
+++ catch-culture-frontend/src/pages/mypage/LikeList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Backitem from '../../components/Backitem';
 import CategorySelector from '../../components/categorySelector/CategorySelector';
 import EventCard from '../../components/eventCard/EventCard';
@@ -42,6 +42,10 @@ function Likes() {
       setDataList(dataList.concat(res.data.content));
     } catch (e) {
       console.log(e);
+      if(e.response.data.code === "LOGIN_FAIL") {
+        alert('로그인 만료! 다시 로그인 해주세요.');
+        navigate(`/`);
+    }
     }
   };
 
