6f0115a04711671c3bd77ccd4b4997060cddba58
jinwoo1234 <skdiwlsdn5@naver.com>
2023-12-07 14:03:37 +0900

    Refactor: fix null image codeSmell

--- catch-culture-frontend/src/pages/visitAuth/VisitAuth.jsx
+++ catch-culture-frontend/src/pages/visitAuth/VisitAuth.jsx
@@ -4,19 +4,7 @@ import * as S from './VisitAuthStyle';
 import Backitem from '../../components/Backitem';
 
 import axios from '../../api/axios';
-
-const ImgSvg = (size) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size.size} height={size.size} viewBox="0 0 38 38" fill="none">
-        <g clipPath="url(#clip0_1_1302)">
-            <path d="M33.25 30.0833V7.91667C33.25 6.175 31.825 4.75 30.0833 4.75H7.91667C6.175 4.75 4.75 6.175 4.75 7.91667V30.0833C4.75 31.825 6.175 33.25 7.91667 33.25H30.0833C31.825 33.25 33.25 31.825 33.25 30.0833ZM13.4583 21.375L17.4167 26.1408L22.9583 19L30.0833 28.5H7.91667L13.4583 21.375Z" fill="#A7A7A7"/>
-        </g>
-        <defs>
-            <clipPath id="clip0_1_1302">
-                <rect width="38" height="38" fill="white"/>
-            </clipPath>
-        </defs>
-    </svg>
-);
+import ImgSvg from '../../components/imgSvg/ImgSvg';
 
 function VisitAuth() {
     const params = useParams();
