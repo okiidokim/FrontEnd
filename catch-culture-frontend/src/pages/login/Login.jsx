7441d1084b49f59fe78261361007cb7b1f4c0662
jinwoo1234 <skdiwlsdn5@naver.com>
2024-05-06 21:53:08 +0900

    Fix: Login 페이지 변경

--- catch-culture-frontend/src/pages/login/Login.jsx
+++ catch-culture-frontend/src/pages/login/Login.jsx
@@ -5,6 +5,9 @@ import LogoImg from '../../assets/images/logo.png';
 import KakaoImg from '../../assets/img/kakao.png';
 import GoogleImg from '../../assets/img/google.png';
 import NaverImg from '../../assets/img/naver.png';
+import titleImg from '../../assets/images/login/title.png'
+import LoginImg from '../../assets/images/login/login_img.png'
+
 import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
@@ -14,7 +17,10 @@ const StyledImg = styled.img`
 function Login() {
   return (
     <div className="loginwrap">
-      <img className="header" src={LogoImg} alt="로고이미지" />
+      <img className="header" src={titleImg} alt="타이틀이미지" />
+
+      <img className="bannerImg" src={LoginImg} alt="배너이미지" />
+
       <div className="buttonlist">
         <Link to={'https://catch-culture.com/oauth2/authorization/kakao'}>
           <button className="kakao">
