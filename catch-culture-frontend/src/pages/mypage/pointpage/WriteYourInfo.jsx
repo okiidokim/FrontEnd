afaa5897cdc4d9ae953411ef7fe67766e682005e
okiidokim <scw09268@dgu.ac.kr>
2024-05-10 10:56:31 +0900

    feat : css 파일 추가

--- catch-culture-frontend/src/pages/mypage/pointpage/WriteYourInfo.jsx
+++ catch-culture-frontend/src/pages/mypage/pointpage/WriteYourInfo.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from '../../../api/axios';
+import React from 'react';
 import Backitem from '../../../components/Backitem';
+import './WriteYourInfo.css';
 
 export default function WriteYourInfo() {
   return (
